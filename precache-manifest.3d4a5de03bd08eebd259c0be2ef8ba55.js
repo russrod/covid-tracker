ce2022e1ee3ff9dbb47dc8047de92d170c66a48c
Russel Rodrigues <russel.rodrigues@sts.in>
2020-04-18 19:43:48 +0530

    Updates

---
+++ precache-manifest.3d4a5de03bd08eebd259c0be2ef8ba55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4419c3457414ef0d6256",
+    "url": "/covid-tracker/static/css/main.2f687ddc.chunk.css"
+  },
+  {
+    "revision": "4419c3457414ef0d6256",
+    "url": "/covid-tracker/static/js/main.4419c345.chunk.js"
+  },
+  {
+    "revision": "bd62c9fc762579f23880",
+    "url": "/covid-tracker/static/js/1.bd62c9fc.chunk.js"
+  },
+  {
+    "revision": "0cdb763e84dcfcd764e7",
+    "url": "/covid-tracker/static/js/runtime~main.0cdb763e.js"
+  },
+  {
+    "revision": "a1e2d857e49517c7a05f107c1204222d",
+    "url": "/covid-tracker/index.html"
+  }
+];
